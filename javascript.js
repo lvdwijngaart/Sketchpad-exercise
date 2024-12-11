7f11b83a08e2c055be9cb4465ab19044d5558f30
lvdwijngaart <l.vd.wijngaart@outlook.com>
2024-12-11 07:05:24 +0100

    Add 2 modes: randomRGB and darkenSquare
    RandomRGB mode will turn a hovered-over square into a random colour and
    DarkenSquare mode will turn a hovered-over square 10% less opaque. The
    activation of modes is done by setting active classes and based on active classes
    the right method is called when hovering over a square.

--- javascript.js
+++ javascript.js
@@ -8,14 +8,46 @@ createGrid(INITIAL_WIDTH);
 let resetButton = document.querySelector("#reset");
 resetButton.addEventListener("click", resetGrid);
 
+let modes = [];
+
+let randomRGBButton = document.querySelector("#randomRGB");
+modes.push(randomRGBButton);
+
+let darkenSquareButton = document.querySelector("#darkenSquare");
+modes.push(darkenSquareButton);
+
+randomRGBButton.addEventListener("click", () => {
+    setActiveMode(randomRGBButton, darkenSquareButton)
+});
+darkenSquareButton.addEventListener("click", () => {
+    setActiveMode(darkenSquareButton, randomRGBButton)
+});
+
+
 function createGrid(width) {
     for (let i = 0; i < width*width; i++) {
         let div = document.createElement("div");
         div.setAttribute("class", "square");
         div.setAttribute("id", i);
-        div.style.cssText = `flex: 0 0 calc(100%/${width});`
+        div.style.cssText = `flex: 0 0 calc(100%/${width})`;
+        div.style.backgroundColor = "#000000";
+        div.style.opacity = 0;
         div.addEventListener("mouseover", () => {
-            setHoverEffect(div);
+            modes.forEach(mode => {
+                if(mode.classList.contains("active")) {
+                    switch (mode.id) {
+                        case "randomRGB": 
+                            setRandomRGBEffect(div);
+                            console.log("RGB is set")
+                            break;
+                        case "darkenSquare": 
+                            setDarkenSquareEffect(div);
+                            break;
+                        default: 
+                            setRandomRGBEffect(div);
+                    }
+                }
+            });
         });
         divs.push(div);
         container.appendChild(div);
@@ -39,7 +71,40 @@ function clearContainerDiv() {
         container.removeChild(element);
     });
 }
+function setRandomRGBEffect(div) {
+    let red = Math.floor((Math.random() * 255));
+    let green = Math.floor((Math.random() * 255));
+    let blue = Math.floor((Math.random() * 255));
+    let opacity = Math.floor((Math.random()*10))/10;
+    let rgbColor = `rgb(${red}, ${green}, ${blue})`;
+    console.log("Random color:", rgbColor, div.style.opacity);  // Debugging output
+    div.style.backgroundColor = rgbColor;
+    div.style.opacity = opacity;
+}
 
-function setHoverEffect(div) {
-    div.style.backgroundColor = "pink";
+
+function setDarkenSquareEffect(div) {
+    // Get the current opacity value from inline style, default to 1 if not set
+    let oldOpacity = parseFloat(div.style.opacity) || 0; 
+
+    // Increase opacity by 0.1, but make sure it doesn't exceed 1
+    let newOpacity = Math.min(oldOpacity + 0.1, 1);
+
+    // Set the new opacity value
+    div.style.opacity = newOpacity;
+
+    // For debugging: Log the old and new opacity values
+    console.log("Old opacity:", oldOpacity);
+    console.log("New opacity:", newOpacity);
+}
+
+function setActiveMode(button, removeClassFromButton) {
+
+    if (!button.classList.contains("active")) {
+        button.classList.add("active");
+        console.log(button.classList);
+    }
+    if (removeClassFromButton.classList.contains("active")) {
+        removeClassFromButton.classList.remove("active");
+    }
 }
